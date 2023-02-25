001c8267295cc5de2a32a74c15485b85193862a9
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-02-25 22:18:40 +0200

    Lab 1 - Create controller ProductController

---
+++ BakeryStore/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using BakeryStore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BakeryStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProducts _products;
+
+        public ProductController(IProducts products) 
+        {
+            _products = products;
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            return View(_products.GetProducts());
+        }
+    }
+}
