8d24fb0808315aedf35e27c85b3feca7f5dbf8e2
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-02-25 22:10:51 +0200

    Lab 1 - Create model Product

---
+++ BakeryStore/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace BakeryStore.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }  
+        public string Photo { get; set; }
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; } = decimal.Zero;
+        public string Stuff { get; set; }
+        public string Calories { get; set; }
+        public double Weight { get; set;}
+        public int ShelfLife { get; set; }
+        public string StorageConditions { get; set; }
+        public bool isFavourite { get; set; }
+        public bool Available { get; set; }
+    }
+}
