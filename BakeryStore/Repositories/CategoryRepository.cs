8a0e9db3507624a6b2e6d41a25e3ce429390f35a
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-04 10:59:55 +0200

    Lab 3 - Create repository CategoryRepository

---
+++ BakeryStore/Repositories/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using BakeryStore.Database;
+using BakeryStore.Interfaces;
+using BakeryStore.Models;
+
+namespace BakeryStore.Repositories
+{
+    public class CategoryRepository : ICategories
+    {
+        private readonly AppDBContent appDBContent;
+        public CategoryRepository(AppDBContent appDBContent)
+        {
+            this.appDBContent = appDBContent;
+        }
+
+        public IEnumerable<Category> GetCategories() => appDBContent.Category;
+        
+        public Category GetCategory(int id) => appDBContent.Category.First(с => с.Id == id);
+    }
+}
