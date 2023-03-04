5efa3125c6b51ba6fe9d50d47ceb02ea55f3b7ee
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-04 11:00:07 +0200

    Lab 3 - Create repository ProductRepository

---
+++ BakeryStore/Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using BakeryStore.Database;
+using BakeryStore.Interfaces;
+using BakeryStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BakeryStore.Repositories
+{
+    public class ProductRepository : IProducts
+    {
+        private readonly AppDBContent appDBContent;
+        public ProductRepository(AppDBContent appDBContent)
+        {
+            this.appDBContent = appDBContent;
+        }
+
+        public Product GetProductById(int id) => appDBContent.Product.Include(c => c.Category).Where(p => p.Id == id).First();
+
+        public IEnumerable<Product> GetProducts() => appDBContent.Product.Include(c =>c.Category);
+
+
+        public IEnumerable<Product> GetProductsByCategory(string categoryName) => appDBContent.Product.Include(c => c.Category).Where(p => p.Category.Name == categoryName);
+
+        public IEnumerable<Product> SearchProductsByName(string productName) => appDBContent.Product.Include(c => c.Category).Where(p => p.Title.Contains(productName));
+    }
+}
