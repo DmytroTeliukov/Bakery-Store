9e9841468f66a0a19b33306738fd8023eb496c10
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-04 10:53:41 +0200

    Lab 3 - Add data annotation to model Category

--- BakeryStore/Models/Category.cs
+++ BakeryStore/Models/Category.cs
@@ -1,7 +1,10 @@
-﻿namespace BakeryStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BakeryStore.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
