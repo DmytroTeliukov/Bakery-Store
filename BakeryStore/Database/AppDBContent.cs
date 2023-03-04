d715ce86417047735a889ec9c7f0ec5490339d11
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-03-04 10:58:39 +0200

    Lab 3 - Create AppDBContent

---
+++ BakeryStore/Database/AppDBContent.cs
@@ -0,0 +1,12 @@
+﻿using BakeryStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BakeryStore.Database
+{
+    public class AppDBContent : DbContext
+    {
+        public AppDBContent(DbContextOptions<AppDBContent> options) : base(options) { }
+        public DbSet<Product> Product{ get; set; }
+        public DbSet<Category> Category { get; set; }
+    }
+}
