967dbff78151067f63fb8915bafd41176620d5af
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-04-16 18:45:58 +0300

    Lab 4 - Change interface in AppDBContent

--- BakeryStore/Database/AppDBContent.cs
+++ BakeryStore/Database/AppDBContent.cs
@@ -1,12 +1,15 @@
 ﻿using BakeryStore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BakeryStore.Database
 {
-    public class AppDBContent : DbContext
+    public class AppDBContent : IdentityDbContext
     {
         public AppDBContent(DbContextOptions<AppDBContent> options) : base(options) { }
+   
         public DbSet<Product> Product{ get; set; }
         public DbSet<Category> Category { get; set; }
+        public DbSet<User> User { get; set; }  
     }
 }
