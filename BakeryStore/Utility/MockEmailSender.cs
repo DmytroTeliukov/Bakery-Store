bb73a801a2417f6d867526131c8fc4915a475b73
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-04-16 18:48:37 +0300

    Lab 4 - mock email sender

---
+++ BakeryStore/Utility/MockEmailSender.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace BakeryStore.Utility
+{
+    // for mocking email sender
+    public class MockEmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
