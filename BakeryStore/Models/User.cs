0990d87e330921a5a2988182cb8d7a4db4cdfebf
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-04-16 18:47:45 +0300

    Lab 4 - Add model User

---
+++ BakeryStore/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Build.Framework;
+
+namespace BakeryStore.Models
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        public string Lastname { get; set; }
+        [Required]
+        public string Firstname { get; set; }
+        public string? StreetAddress { get; set; }
+        public string? City { get; set; }
+        public string? PostalCode { get; set; }
+
+    }
+}
