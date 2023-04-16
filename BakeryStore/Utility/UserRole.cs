ce05f6801c9c5614bf07417708df8558b825eaa2
Dmytro Teliukov <dmytro.teliukov@nure.ua>
2023-04-16 18:48:24 +0300

    Lab 4 - Add UserRole

---
+++ BakeryStore/Utility/UserRole.cs
@@ -0,0 +1,9 @@
+﻿namespace BakeryStore.Utility
+{
+    public static class UserRole
+    {
+        public const string RoleCustomer = "Customer";
+        public const string RoleAdmin = "Admin";
+        public const string RoleEmployee = "Employee";
+    }
+}
